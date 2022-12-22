539cb907fb5c8ce083b631bc913d932f5fb05371
SehribanTas <103320604+SehribanTas@users.noreply.github.com>
2022-12-22 15:04:28 +0300

    feature
    feautue : added ui

--- to_do_flutter/lib/main.dart
+++ to_do_flutter/lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'todo_app.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoApp(),
     );
   }
 }
---
+++ to_do_flutter/lib/todo_app.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_flutter/widgets/title_widget.dart';
+import 'package:to_do_flutter/widgets/todo_list_item_widget.dart';
+import 'package:to_do_flutter/widgets/toolbar_widget.dart';
+import 'package:uuid/uuid.dart';
+
+import 'models/todo_model.dart';
+
+
+class TodoApp extends StatelessWidget {
+  TodoApp({Key? key}) : super(key: key);
+
+  final newTodoController = TextEditingController();
+  List<TodoModel> allTodos = [
+    TodoModel(id: const Uuid().v4(), description: 'Spora Git'),
+    TodoModel(id: const Uuid().v4(), description: 'Alışveriş yap'),
+    TodoModel(id: const Uuid().v4(), description: 'Ders Çalış'),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        children: [
+          const TitleWidget(),
+          TextField(
+            controller: newTodoController,
+            decoration:
+                const InputDecoration(labelText: 'Neler Yapacaksın Bugün ?'),
+            onSubmitted: (newTodo) {
+              debugPrint('şunu ekle $newTodo');
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const ToolBarWidget(),
+          for (var i = 0; i < allTodos.length; i++)
+            Dismissible(
+                key: ValueKey(allTodos[i].id),
+                onDismissed: (_){
+                  
+                },
+                child: TodoListItemWidget(item: allTodos[i]))
+        ],
+      ),
+    );
+  }
+}
